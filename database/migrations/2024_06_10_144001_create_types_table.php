6a35964f6412017c470c234ccd25b48ce7c9a5b3
hajjamn <hajjamn@gmail.com>
2024-06-10 16:49:06 +0200

    ADD: types migration

--- database/migrations/2024_06_10_144001_create_types_table.php
+++ database/migrations/2024_06_10_144001_create_types_table.php
@@ -12,8 +12,8 @@ return new class extends Migration {
     {
         Schema::create('types', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->text('description');
+            $table->string('name', '50');
+            $table->text('description')->nullable();
             $table->timestamps();
         });
     }
